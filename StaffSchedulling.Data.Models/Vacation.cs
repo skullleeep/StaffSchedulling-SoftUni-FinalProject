8e9e23cc48a6e2bcd45f96218a3a2398a38adb18
skullleeep <skullleep@gmail.com>
2024-12-08 16:47:37 +0200

    Added Unique Index to Entity Vacation in the DB

--- StaffSchedulling.Data.Models/Vacation.cs
+++ StaffSchedulling.Data.Models/Vacation.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StaffScheduling.Data.Models
 {
+    [Index(nameof(EmployeeId), nameof(StartDate), nameof(EndDate), IsUnique = true)]
     public class Vacation
     {
         [Key]
---
+++ StaffSchedulling.Data/Migrations/20241208144704_AddUniqueIndexOfPropertiesEmployeeIdAndStartDateAndEndDateToEntityVacation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StaffScheduling.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexOfPropertiesEmployeeIdAndStartDateAndEndDateToEntityVacation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vacations_EmployeeId",
+                table: "Vacations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_EmployeeId_StartDate_EndDate",
+                table: "Vacations",
+                columns: new[] { "EmployeeId", "StartDate", "EndDate" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vacations_EmployeeId_StartDate_EndDate",
+                table: "Vacations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_EmployeeId",
+                table: "Vacations",
+                column: "EmployeeId");
+        }
+    }
+}
